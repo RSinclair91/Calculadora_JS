64839f2a9e9a8c2addeffb9c53cd33313bf487cf
Roberto <s.c.roberto.rs@gmail.com>
2022-06-17 18:23:08 -0300

    calculadora 2

--- Display.js
+++ Display.js
@@ -3,11 +3,27 @@ class Display{
         this.displayValorActual = displayValorActual;
         this.displayValorAnterior = displayValorAnterior;
         this.calculadora = new Calculadora();
+        this.tipoOperacion = undefined;
         this.valorActual = '';
         this.valorAnterior = '';
     }
+
+    borrar(){
+        this.valorActual = this.valorActual.toString().slice(0,-1);
+        this.imprimirValores();
+    }
+
+    borrarTodo(){
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
+    }
+
     agregarNumero(numero) {
-        this.valorActual = numero;
+        if(numero === '.' && this.valorActual.includes('.')) return 
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.imprimirValores();
         
     }
 
@@ -15,4 +31,13 @@ class Display{
         this.displayValorActual.textContent = this.valorActual;
         this.displayValorAnterior.textContent = this.valorAnterior;
     }
+
+    calcular(){
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if( isNaN (valorActual) || isNaN (valorAnterior) ) return
+        this.valorActual = this.calculadora[this.tipoOperacion](valorAnterior, valorActual);
+    }
+
 }

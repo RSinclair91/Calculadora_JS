f016618c931321ffc0c3ee1bd84c9ec1b6bbf750
Roberto <s.c.roberto.rs@gmail.com>
2022-06-17 09:41:42 -0300

    calculadora

---
+++ index.js
@@ -0,0 +1,10 @@
+const displayValorAnterior = document.getElementById('valorAnterior');
+const displayValorActual = document.getElementById('valorActual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll ('.operador')
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton =>{
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
